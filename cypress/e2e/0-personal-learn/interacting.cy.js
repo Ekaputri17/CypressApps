8aef017fe2d0834e4566b9797de6023a998ee1fa
Ekaputri17 <ekaputriw1@gmail.com>
2023-04-17 21:33:47 +0700

    menambahkan file test.cy.js

---
+++ cypress/e2e/0-personal-learn/interacting.cy.js
@@ -0,0 +1,13 @@
+// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+  it('Visit the website', () => {
+    cy.visit('http://zero.webappsecurity.com/login.html')
+    cy.url().should('include', 'login.html')
+})
+
+  it('Should fill username', () => {
+    cy.get('#user_login').clear()
+    cy.get('#user_login').type('username')
+  })
+})
