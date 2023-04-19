b0d55fa8136faef1d08c26449ba054afb47de360
Ekaputri17 <ekaputriw1@gmail.com>
2023-04-19 23:14:39 +0700

    update interacting.cy.js

--- cypress/e2e/0-personal-learn/interacting.cy.js
+++ cypress/e2e/0-personal-learn/interacting.cy.js
@@ -2,12 +2,14 @@
 
 describe('Working with inputs', () => {
   it('Visit the website', () => {
-    cy.visit('http://zero.webappsecurity.com/login.html')
-    cy.url().should('include', 'login.html')
-})
+    cy.visit('http://zero.webappsecurity.com/login.html');
+    
+    cy.get("#user_login").type("username");
+    cy.get("#user_password").type("password");
 
-  it('Should fill username', () => {
-    cy.get('#user_login').clear()
-    cy.get('#user_login').type('username')
-  })
-})
+    cy.get("#user_remember_me").type("_remember_me");
+
+    cy.get("input[type=submit]").click();
+
+  });
+});
