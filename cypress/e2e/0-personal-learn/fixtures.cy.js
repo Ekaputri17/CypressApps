b4466e7023bfbc4712408499a43d887e4b7b5d8f
Ekaputri17 <ekaputriw1@gmail.com>
2023-05-10 21:37:36 +0700

    memperbarui pada proses pembelian menggunakan akun standard_user

--- cypress/e2e/0-personal-learn/fixtures.cy.js
+++ cypress/e2e/0-personal-learn/fixtures.cy.js
@@ -1,21 +1,6 @@
 
 describe("Test login", function () {
-    it("Berhasil login menggunakan akun standard_user", function () {
-     cy.fixture("user").then(function (data) {
-      cy.visit("https://www.saucedemo.com/");
-  
-      cy.get("#user-name").type(data.daftar_user.standard.username);
-      
-      cy.get("#password").type(data.daftar_user.standard.password);
-      
-      cy.get("#login-button").click();
-    
-      // assertions
-      cy.url().should("include", "inventory.html");
 
-    });
-  });
-  
   it("Gagal login menggunakan akun locked_out_user", function () {
     cy.fixture("user").then(function (data) {
      cy.visit("https://www.saucedemo.com/");
@@ -30,33 +15,30 @@ describe("Test login", function () {
      cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
     });
   });
-  it("Berhasil login menggunakan akun problem_user", function () {
-    cy.fixture("user").then(function (data) {
-     cy.visit("https://www.saucedemo.com/");
- 
-     cy.get("#user-name").type(data.daftar_user.problem.username);
-     
-     cy.get("#password").type(data.daftar_user.problem.password);
-     
-     cy.get("#login-button").click();
-   
-     // assertions
-     cy.url().should("include", "inventory.html");
-    });
-  });
+  it("Berhasil melakukan pembelian menggunakan akun standard_user", function () {
+     cy.fixture("user").then(function (data) {
+      cy.visit("https://www.saucedemo.com/");
+  
+      cy.get("#user-name").type(data.daftar_user.standard.username);
+      
+      cy.get("#password").type(data.daftar_user.standard.password);
+      
+      cy.get("#login-button").click();
 
-  it("Berhasil login menggunakan akun performance_glitch_user", function () {
-    cy.fixture("user").then(function (data) {
-     cy.visit("https://www.saucedemo.com/");
- 
-     cy.get("#user-name").type(data.daftar_user.performance_glitch.username);
-     
-     cy.get("#password").type(data.daftar_user.performance_glitch.password);
-     
-     cy.get("#login-button").click();
-   
-     // assertions
-     cy.url().should("include", "inventory.html");
+      cy.get("#item_1_img_link").click()
+      cy.get("#add-to-cart-sauce-labs-bolt-t-shirt").click()
+      cy.get("#back-to-products").click()
+      cy.get("#shopping_cart_container").click()
+      cy.get("#checkout").click()
+      cy.fixture("user").then(function (data) {
+        cy.get("#first-name").type(data.daftar_user.standard.firstname);
+        cy.get("#last-name").type(data.daftar_user.standard.lastname);
+        cy.get("#postal-code").type(data.daftar_user.standard.zip);
+      cy.get("#continue").click()
+      cy.get("#finish").click()
+      cy.get("#react-burger-menu-btn").click()
+      cy.get("#logout_sidebar_link").click()
+      })
     });
   });
 });
